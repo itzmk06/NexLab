a5dc3d88e81b8e545900e553f16c34a12a68bb0f
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-09-28 09:12:36 +0530

    implemented theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs'
-import type {Metadata} from 'next';
-import {Raleway,Macondo,Poppins} from 'next/font/google'
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Raleway, Macondo, Poppins } from "next/font/google";
+import ThemeProvider from "@/context/ThemeProvider";
 
-const raleway=Raleway({
-  subsets:['latin'],
-  weight:['100','200','300','400','500','600','700'],
-  variable:'--font-raleway'
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  variable: "--font-raleway",
 });
 
-const macondo=Macondo({
-  subsets:['latin'],
-  weight:'400',
-  variable:'--font-macondo'
+const macondo = Macondo({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-macondo",
 });
 
-const poppins=Poppins({
-  subsets:['latin'],
-  weight:'400',
-  variable:'--font-poppins'
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-poppins",
 });
 
-
-
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: "NexLab",
-  description: "Where developers unite to innovate. Empowering collaboration with cutting-edge tools for seamless project creation. Join the future of development!",
-  icons:{
-    icon:'/assets/nexlab.png'
-  }
+  description:
+    "Where developers unite to innovate. Empowering collaboration with cutting-edge tools for seamless project creation. Join the future of development!",
+  icons: {
+    icon: "/assets/nexlab.png",
+  },
 };
 
 export default function RootLayout({
@@ -38,25 +38,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      elements:{
-        formButtonPrimary:'primary-gradient',
-        footerActionLink:'primary-text-gradient hover:text-primary-500'
-      }
-    }}>
     <html lang="en">
-      <body className={`${macondo.variable} ${raleway.variable} ${poppins.variable}`}>
-        <header>
-            {/* <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-            <UserButton />
-            </SignedIn> */}
-          </header>
-        {children}
+      <body
+        className={`${macondo.variable} ${raleway.variable} ${poppins.variable}`}
+      >
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
