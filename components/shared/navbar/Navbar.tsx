bb4fb051985f42d94301c091297c440d9ba5b0df
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-09-28 16:51:20 +0530

    added themes option

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,23 +1,27 @@
+"use client";
 import { SignedIn,UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image"
+import Theme from "./Theme";
+import { useTheme } from "@/context/ThemeProvider";
 
 export default function Navbar(){
+    const {mode}=useTheme();
     return(
         <nav className="flex-between background-light900_dark200 fixed z-50 w-full  gap-5 p-2 shadow-light-300 dark:shadow-none sm:px-12">
-        <Link href='/dashboard' className="flex items-center  md:pl-0">
+        <Link href='/dashboard' className="flex items-center gap-[0.1rem] md:pl-0">
             <Image className="w-7 md:w-8"
-            src="/assets/nexlab.png"
-            width={32}
-            height={32}
+            src={`${mode==='dark'?'/assets/nexlab-light.png':'/assets/nexlab.png'}`}
+            width={25}
+            height={25}
             alt='NexLab'
             />
             <p className="h2-bold text-dark-100 dark:text-light-900 max-sm:hidden">exLab</p>
         </Link>
         GlobalSearch
         <div className="flex-between gap-3">
-            Theme
+            <Theme/>
             <SignedIn>
                 <UserButton
                     appearance={{
---
+++ constants/constant.ts
@@ -0,0 +1,4 @@
+export const themesBrowser = [
+    { value: "light", label: "Light", icon: "/assets/icons/sun.svg" },
+    { value: "dark", label: "Dark", icon: "/assets/icons/moon.svg" },
+  ];
