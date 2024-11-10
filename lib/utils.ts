13198c2038d3e0e1e6e21842ee485e7df40d74ea
Manoj Kumar M <itzmk06@gmail.com>
2024-11-10 18:20:03 +0530

    eslint fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -46,6 +46,6 @@ export const formatAndDivideNumber = (num: number): string => {
     const formattedNum = (num / 1000).toFixed(1);
     return `${formattedNum}K`;
   } else {
-    // return num.toString();
+    return num.toString();
   }
 };
